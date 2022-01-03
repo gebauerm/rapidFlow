6a5291ae9e4566de1fe367a1f947a7fe8ed51d40
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-01-03 15:35:43 +0100

    adjusted setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='rapidflow',
     version='0.1',
-    description='A place to perform micro experimentation fast with easy scaling.',
+    description='rapidFlow - A framework to perform micro experimentation fast with easy scaling.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     author='Michael Gebauer',
     author_email='gebauerm23@gmail.com',
@@ -12,14 +18,14 @@ setup(
     install_requires=[
         "optuna==2.9.1",
         "click==8.0.1", "scikit-learn==0.24.2", "scipy==1.7.0", "networkx==2.5.1", "psycopg2-binary",
-        "docker==5.0.3", "pytest==6.2.5", "pandas==1.3.5"],
+        "docker==5.0.3", "pytest==6.2.5", "pandas==1.3.5", "torch", "tqdm==4.62.3"],
     dependency_links=[""],
     python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ]
 )
