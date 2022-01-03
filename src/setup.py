c3bb2095f8306f26cb1c6ad86d6a6c3983b58294
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-01-03 18:56:19 +0100

    changed version number

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ with open(path, "r") as fh:
 
 setup(
     name='rapidflow',
-    version='0.1.1',
+    version='0.1.2',
     description='rapidFlow - A framework to perform micro experimentation fast with easy scaling.',
     long_description=long_description,
     long_description_content_type='text/markdown',
