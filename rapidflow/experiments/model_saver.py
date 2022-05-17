4ce53642bfbfe8db7986af4dfe5fb222d6bc1e04
fmaschhur <f.maschhur@live.de>
2022-05-17 17:58:10 +0200

    this commit implements the basic structure of the ModelSaver class and its strategies

---
+++ rapidflow/experiments/model_saver.py
@@ -0,0 +1,44 @@
+import os
+
+import torch
+from joblib import dump
+
+
+class ModelSaver:
+    def __init__(self):
+        self.strategies = {
+            torch.nn.Module: TorchStrategy,
+
+        }
+        self.strategy = None
+
+    def set_strategy(self, model):
+        try:
+            self.strategy = self.strategies[type(model)]
+        except KeyError:
+            print(Warning(f"Your model is of type {type(model)}."
+                          f"Currently saving is only supported for torch and sklearn models!"))
+
+    def save_model(self, model, title, path):
+        try:
+            self.strategy.execute(model, title, path)
+        except AttributeError:
+            print(Warning("You have not set a strategy! Please call set_strategy() first!"))
+
+
+class SavingStrategy:
+    @staticmethod
+    def execute(model, title, path):
+        raise NotImplementedError
+
+
+class TorchStrategy(SavingStrategy):
+    @staticmethod
+    def execute(model, title, path):
+        torch.save(model.state_dict(), os.path.join(path, f"{title}.pt"))
+
+
+class SKLearnStrategy(SavingStrategy):
+    @staticmethod
+    def execute(model, title, path):
+        dump(model, os.path.join(path, f"{title}.gzip"))
--- rapidflow/experiments/study_result_saver.py
+++ rapidflow/experiments/study_result_saver.py
@@ -1,17 +1,19 @@
-import torch
 import json
 import shutil
 import os
+
+from rapidflow.experiments.model_saver import ModelSaver
 
 
 class StudyResultSaver:
 
     def __init__(self, experiment_path) -> None:
         self.experiment_path = experiment_path
+        self.model_saver = ModelSaver()
 
     def save_best_model(self, idx, model, hyperparameters, path):
         """
-        Saves the best model of a study of the experiment class. Currently it only saves torch models.
+        Saves the best model of a study of the experiment class. Currently it only saves torch and sklearn models.
 
         Args:
             idx ([type]): [description]
@@ -19,14 +21,9 @@ class StudyResultSaver:
             hyperparameters ([type]): [description]
             path ([type]): [description]
         """
-        try:
-            self.save_torch_model(model, f"best_model_{idx}", path)
-        except AttributeError:
-            print(Warning("Your model is not a torch model. Currently saving for non torch models is not supported!"))
+        self.model_saver.set_strategy(model)
+        self.model_saver.save_model(model, f"best_model_{idx}", path)
         self.save_hyperparameters(hyperparameters, f"best_model_parameters_{idx}", path)
-
-    def save_torch_model(self, model, title, path):
-        torch.save(model.state_dict(), os.path.join(path, f"{title}.pt"))
 
     def save_hyperparameters(self, hyperparameters, title, path):
         with open(os.path.join(path, f"{title}.json"), "w") as f:
